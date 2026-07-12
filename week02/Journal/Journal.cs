1f27ba790c961e7bed8e265161eebda6c220cdfb
catayocgladys-cpu <gcatayoc@byupathway.edu>
2026-07-11 20:24:02 -0600

    Fixed the bug for Journal.cs for Journal

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,62 @@
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Journal
+{
+    private List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry entry)
+    {
+        _entries.Add(entry);
+    }
+
+    public void DisplayAll()
+    {
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+
+    public void SaveToFile(string file)
+    {
+        // Use block automatically closes and saves the file when finished
+        using (StreamWriter outputFile = new StreamWriter(file))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date}|{entry._promptText}|{entry._entryText}");
+            }
+        }
+        Console.WriteLine("Journal saved successfully.");
+    }
+
+    public void LoadFromFile(string file)
+    {
+        // clear out current runtime entries before loading the file data
+        _entries.Clear();
+
+        string[] lines = System.IO.File.ReadAllLines(file);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(new string[] { "|" }, StringSplitOptions.None);
+
+            if (parts.Length == 3)
+            {
+                Entry loadedEntry = new Entry();
+                // assign the components 
+                loadedEntry._date = parts[0];
+                loadedEntry._promptText = parts[1];
+                loadedEntry._entryText = parts[2];
+
+                _entries.Add(loadedEntry);
+            }
+        }
+
+        Console.WriteLine("Journal loaded successfully.");
+    }
+                
+}
