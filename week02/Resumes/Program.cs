5fc9ae3d48120eb1a369fdd9ab5e098ced842ca4
catayocgladys-cpu <gcatayoc@byupathway.edu>
2026-07-11 18:49:16 -0600

    Completed Week 2 Abstraction Exercise

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // Create and populate the first job object
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        // Create and populate the second job object
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Create the master Resume Object
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        // Layer the object by adding the jobs into the resume list
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Execute the clean abstraction display
+        myResume.Display();
+        
     }
+    
 }
