15cae047c7f3643ee257be780599b93774706c30
catayocgladys-cpu <gcatayoc@byupathway.edu>
2026-07-16 10:41:06 -0600

    done week 2

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,31 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Create and populate the first job object
+        Resume myResume = new Resume();
+        myResume._name = "John Doe";
+
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
+        job1._company = "Tech Company";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
 
-        // Create and populate the second job object
         Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
+        job2._jobTitle = "Senior Developer";
+        job2._company = "Another Tech Company";
+        job2._startYear = 2023;
+        job2._endYear = 2025;
 
-        // Create the master Resume Object
         Resume myResume = new Resume();
         myResume._name = "Allison Rose";
 
-        // Layer the object by adding the jobs into the resume list
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
 
-        // Execute the clean abstraction display
         myResume.Display();
-        
     }
-    
 }
